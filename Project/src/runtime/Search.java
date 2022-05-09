ca802c4bf4f0ae1e8a5c84d27442c53c2f51543f
Zakilim <zzqandlth@outlook.com>
2022-05-09 22:28:08 +0800

    1. 增加对文件和文件路径下所有文件的读取 2. 增加Expr的匹配支持

--- Project/src/runtime/Search.java
+++ Project/src/runtime/Search.java
@@ -1,8 +1,13 @@
 package runtime;
 
+import AstGenerator.AstInfo;
+import Visitor.ExpVisitor;
 import org.antlr.v4.runtime.ParserRuleContext;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Search {
     /*
@@ -10,12 +15,44 @@ public class Search {
     返回值类型待定
     可能要同时记录所属文件, 可以使用/完善utils/SourcePosition类
      */
+
+    private static List<AstInfo> readFile(String path) throws IOException {
+        File file = new File(path);
+        List<AstInfo> result = new ArrayList<>();
+        if (!file.isDirectory()){
+            if (file.getName().contains(".java"))
+                result.add(new AstInfo(path));
+        } else if (file.isDirectory()){
+            String [] s = file.list();
+            if (s == null)
+                throw new IOException("Empty Directory");
+            for (var f : s){
+                var fileItem = new File(path + f);
+                if (fileItem.isDirectory()){
+                    readFile(fileItem.getAbsolutePath());
+                } else {
+                    if (fileItem.getName().contains(".java"))
+                        result.add(new AstInfo(fileItem));
+                }
+            }
+        }
+        if (result.size() == 0) {
+            throw new IOException("Invalid file name/path");
+        }
+        return result;
+    }
     public static ArrayList<ParserRuleContext> execSearch(CLIParseInfo cliInfo) {
         ArrayList<ParserRuleContext> results = new ArrayList<>();
 
         // TODO: 打开 路径下所有文件 / 文件
-        // TODO: 对每个文件分别查询
-
+        List<AstInfo> astList;
+        try {
+            astList = readFile(cliInfo.path);
+            // TODO: 对每个文件分别查询
+        }catch (IOException e){
+            System.err.println(e.getLocalizedMessage());
+            return null;// a bit ugly
+        }
         switch (cliInfo.target) {
             // TODO: 语句查询
             case "if" -> {
@@ -46,7 +83,14 @@ public class Search {
 
             // TODO: 表达式查询
             case "expr" -> {
-
+                var exprVisitor = new ExpVisitor();
+                exprVisitor.patternPreCompile(cliInfo.expr);
+                List<ParserRuleContext> result = new ArrayList<>();
+                for (var ast : astList) {
+                    var res = exprVisitor.visitCompilationUnit(ast.getRoot());
+                    res = exprVisitor.filter(res, ast.getTokenStream());
+                    result.addAll(res);
+                }
             }
         }
         return results;
