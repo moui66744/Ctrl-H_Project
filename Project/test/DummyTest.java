61d3472e8f80b901b2f69e3e62c2f39118f09030
moui66744 <whaoyu09@163.com>
2022-05-24 22:35:41 +0800

    完善DummyTest, 支持第二阶段测试

--- Project/test/DummyTest.java
+++ Project/test/DummyTest.java
@@ -1,11 +1,21 @@
-// a single test containing all the situations need to be realized in stage 1
+// a single test containing all the situations need to be realized in stage 1 & 2
+
+
+import JavaParser.JavaBaseVisitor;
+import JavaParser.JavaParser;
+import JavaParser.JavaParser.StatementContext;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //test class declaration
-public class DummyTest {
+public class DummyTest extends Class1 implements impl1, impl2 {
     final int data = 0;
     final int a[] /* test */ = {0, 1, 2, 3};
-
+    var ab = 123;
     public static int retinput(int input) {
+        int i;
         return input;
     }
 
@@ -23,6 +33,9 @@ public class DummyTest {
 
 //        test "for"
         for (i = 0; i < 5; i++) {
+            if (a == 0) {
+                a = 0;
+            }
             System.out.print(i);
         }
         System.out.print("\n");
@@ -36,6 +49,7 @@ public class DummyTest {
             while (j < 10) {
                 System.out.print(j);
                 j++;
+                continue;
             }
         }
         System.out.print("\n");
@@ -45,6 +59,10 @@ public class DummyTest {
         do {
             System.out.print(i);
             i++;
+            if (a == 0) {
+                a = 0;
+                break;
+            }
         }
         while (i < 5);
         System.out.print("\n");
@@ -83,6 +101,9 @@ public class DummyTest {
             }
             default: {
                 fun();
+                if (a == 0) {
+                    assert a == 0;
+                }
                 break;
             }
         }
@@ -93,5 +114,34 @@ public class DummyTest {
         } else {
             doSth();
         }
+
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                }
+            }
+            if (d == 0) {
+            }
+        }
+
+        if (a == 0) {
+            if (b == 0) {
+            }
+        }
+
+        if (a == 0) {
+            if (c == 0) {
+            }
+        }
+
+        for(int i=0; i<1; i++)
+            i++;
+
+    }
+}
+
+public interface inter1 extends inter2 {
+    public static void main(String[] args) {
+        System.out.printf("This is inter1");
     }
 }
